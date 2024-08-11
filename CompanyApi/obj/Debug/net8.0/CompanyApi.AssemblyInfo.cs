78894e0aa4bcd80e5c8bfefb59e26c5443bbbdf3
direct777 <sportlake@mail.ru>
2024-08-11 18:31:05 +0300

    MySql tables and data added

--- CompanyApi/obj/Debug/net8.0/CompanyApi.AssemblyInfo.cs
+++ CompanyApi/obj/Debug/net8.0/CompanyApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompanyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+958ed0d366d812d4bfb2b967501f6a4350f89efc")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompanyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompanyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
